66fb5e9a7752772dbb812e40ec669f8b274f6003
Zidni Mubarock <zidmubarock@gmail.com>
2013-01-13 22:28:27 +0700

    typo

--- db/migrate/20130110143847_add_time_track_to_absens.rb
+++ db/migrate/20130110143847_add_time_track_to_absens.rb
@@ -1,6 +1,6 @@
 class AddTimeTrackToAbsens < ActiveRecord::Migration
   def change
     add_column :absens, :onduty, :datetime
-    add_column :absens, :offduty, :detetime
+    add_column :absens, :offduty, :datetime
   end
 end
